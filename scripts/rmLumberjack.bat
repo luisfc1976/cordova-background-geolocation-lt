140b0a63d1cf351c88f1818294011cda7c087685
luisfc1976 <97965097+luisfc1976@users.noreply.github.com>
2022-03-16 18:15:38 +0000

    Update rmLumberjack.bat

--- scripts/rmLumberjack.bat
+++ scripts/rmLumberjack.bat
@@ -5,19 +5,21 @@
 // add your plugins to this list--either 
 // the identifier, the filesystem location 
 // or the URL
-var pluginlist = [
-    "https://github.com/CocoaLumberjack/CocoaLumberjack.git"
-];
+//var pluginlist = [
+//    "https://github.com/CocoaLumberjack/CocoaLumberjack.git"
+//];
 
-var fs = require('fs');
-var path = require('path');
-var sys = require('sys')
-var exec = require('child_process').exec;
+//var fs = require('fs');
+//var path = require('path');
+//var sys = require('sys')
+//var exec = require('child_process').exec;
 
-function puts(error, stdout, stderr) {
-    sys.puts(stdout)
-}
+//function puts(error, stdout, stderr) {
+//    sys.puts(stdout)
+//}
 
-pluginlist.forEach(function(plug) {
-    exec("cordova plugin rm " + plug, puts);
-});
+//pluginlist.forEach(function(plug) {
+//    exec("cordova plugin rm " + plug, puts);
+//});
+
+exec("gem list --local | grep cocoapods )"
