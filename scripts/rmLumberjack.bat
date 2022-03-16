461618b18cb2df6828124d03a396df9aac6eda04
luisfc1976 <97965097+luisfc1976@users.noreply.github.com>
2022-03-16 17:57:35 +0000

    Create rmLumberjack.bat

---
+++ scripts/rmLumberjack.bat
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+//this hook removes cocoaLumberJack
+
+// add your plugins to this list--either 
+// the identifier, the filesystem location 
+// or the URL
+var pluginlist = [
+    "https://github.com/CocoaLumberjack/CocoaLumberjack.git"
+];
+
+var fs = require('fs');
+var path = require('path');
+var sys = require('sys')
+var exec = require('child_process').exec;
+
+function puts(error, stdout, stderr) {
+    sys.puts(stdout)
+}
+
+pluginlist.forEach(function(plug) {
+    exec("cordova plugin rm " + plug, puts);
+});
